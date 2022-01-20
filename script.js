ec2bb94c94b98cb1480d65965d225ff6b1aeda4d
entwinedjet274 <87310419+entwinedjet274@users.noreply.github.com>
2022-01-20 14:27:46 -0600

    update2

--- script.js
+++ script.js
@@ -1,5 +1,65 @@
-document.getElementById("myButton").onclick = function(){
+var storedItem = localStorage.getItem("storedItem");
 
-    var myName = document.getElementById("myText").value;
-    console.log("Hello",myName);
+var storedItem2 = localStorage.getItem("storedItem2");
+
+var storedItem3 = localStorage.getItem("storedItem3");
+
+var storedItem4 = localStorage.getItem("storedItem4");
+
+
+function checkedIn() {
+ 
+  var Item = document.getElementById("input").value;
+
+  var Item2 = document.getElementById("input2").value;
+
+  var array =  [Item + " " + Item2];
+
+
+
+
+
+
+localStorage.setItem( "storedItem", array);
+
+document.getElementById("nameSaved").innerHTML = Item + " " + Item2;
+
+
+let storedItem3 = storedItem;
+
+document.getElementById("nameSaved3").innerHTML = array;
+
+
+
 }
+
+function checkedOut() {
+ 
+  var Item = document.getElementById("input").value;
+
+  var Item2 = document.getElementById("input2").value;
+
+  var array =  [Item + " " +Item2];
+
+localStorage.setItem("storedItem2", array);
+
+document.getElementById("outSaved").innerHTML = Item + " " + Item2;
+
+
+}
+
+
+function get(){
+
+  localStorage.getItem("storedItem");
+  localStorage.getItem("storedItem2");
+  localStorage.getItem("storedItem3");
+  localStorage.getItem("storedItem4");
+
+  document.getElementById("nameSaved2").innerHTML = storedItem; 
+
+   document.getElementById("outSaved2").innerHTML = storedItem2; 
+
+}
+
+   
