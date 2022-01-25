e36650fe17ddbe4f267005038091258d43411f22
entwinedjet274 <87310419+entwinedjet274@users.noreply.github.com>
2022-01-25 12:35:55 -0600

    update 4

--- script.js
+++ script.js
@@ -2,86 +2,45 @@ var storedItem = localStorage.getItem("storedItem");
 
 var storedItem2 = localStorage.getItem("storedItem2");
 
-var storedItem3 = localStorage.getItem("storedItem3");
-
-var storedItem4 = localStorage.getItem("storedItem4");
-  
-
-
-
-function checkedIn() {
- 
-  var Item = document.getElementById("input").value;
-
-  var Item2 = document.getElementById("input2").value;
-
-
-var array =  [Item + " " + Item2];
-
-
-
-
-localStorage.setItem( "storedItem", array);
-
-document.getElementById("nameSaved").innerHTML = Item + " " + Item2;
-
-
-
-
-let storedItem3 = storedItem;
-
-document.getElementById("nameSaved3").innerHTML = array;
-
-storedItem3.push = storedItem4;
-
-
-}
-
-function checkedOut() {
- 
-  var Item = document.getElementById("input").value;
-
-  var Item2 = document.getElementById("input2").value;
-
-  var array =  [Item + " " +Item2];
-
-localStorage.setItem("storedItem2", array);
-
-document.getElementById("outSaved").innerHTML = Item + " " + Item2;
-
-
-}
-
-
-function get(){
-
-  localStorage.getItem("storedItem");
-  localStorage.getItem("storedItem2");
-  localStorage.getItem("storedItem3");
-  localStorage.getItem("storedItem4");
-
-  document.getElementById("nameSaved2").innerHTML = storedItem; 
-
-   document.getElementById("outSaved2").innerHTML = storedItem2; 
-
-}
-
-
-
-
-
 let saveEl = document.getElementById("save-el");
 let countEl = document.getElementById("count-el");
 
 
 console.log(saveEl);
 console.log(countEl);
-let count = "nameSaved";
-let saveBTN = 0;
-let word = 0;
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
 function increment() {
-    count += 1;
-    countEl.innerText = count;
+    var Item = document.getElementById("input").value;
+
+  var Item2 = document.getElementById("input2").value;
+
+  var array =  [Item + " " +Item2];
+let count = array;
+    let rer = count + " " + " - "  ;
+   
+    saveEl.innerText += rer;
+  
+    console.log(count);
+    
+      localStorage.setItem( "storedItem", array);
+
+document.getElementById("nameSaved").innerHTML = array;
+
+
+  
 };
 
 function save() {
@@ -90,19 +49,37 @@ function save() {
 
   var Item2 = document.getElementById("input2").value;
 
-
-
-
+  var array =  [Item + " " +Item2];
+let count = array;
     let rer = count + " - " + " ";
    
-    saveEl.innerText = rer;
+    countEl.innerText += rer;
   
     console.log(count);
     
-    localStorage.setItem( "storedItem", array);
+      localStorage.setItem( "storedItem", array);
 
-document.getElementById("nameSaved").innerHTML = [Item + " " + Item2];
+document.getElementById("nameSaved").innerHTML = array;
+
+
 };
+
+
+
+
+
+function get(){
+
+  localStorage.getItem("storedItem");
+  localStorage.getItem("storedItem2");
+
+
+  document.getElementById("nameSaved2").innerHTML = storedItem; 
+
+   document.getElementById("outSaved2").innerHTML = storedItem2; 
+
+}
+
 
 
 
